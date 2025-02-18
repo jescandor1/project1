f2c709e42ba599c968978436e1c9b87c13fe1a09
jescandor1 <jescandor1@ufl.edu>
2025-02-18 15:58:31 -0500

    added jupyter file to repository created left_endpoint function edited left_endpoint

--- riemann.py
+++ riemann.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc)->float:
     """
-    :param x_vals:
-    :param func:
+    :param x_vals: a one-dimensional NumPy array containing the x-values used in approximating the integral
+    :param func: function to approximate the integral
     :return: sum approximation using the left endpoint
     """
     difference = np.diff(x_vals)
