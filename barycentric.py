a5a5e34a89f5b0187d2a1f7d18bd875755b5fd83
jescandor1 <jescandor1@ufl.edu>
2025-02-20 14:38:41 -0500

    made comments on get_barycentric_coordinates

--- barycentric.py
+++ barycentric.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-def get_barycentric_coordinates(triangle_coordinates: [], point_coordinates: [] ) -> []:
+def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinates: np.array ) -> np.array:
+    """
+    :param triangle_coordinates: 2x3 array (coeff matrix)
+    :param point_coordinates: 1d array (x, y)
+    :return: 1d array (lambda1, lambda2, lambda3)
+    """
     triangle_coordinates += [1, 1, 1]
     point_coordinates += [1]
     barycentric_coordinates = np.linalg.solve(triangle_coordinates, point_coordinates)
