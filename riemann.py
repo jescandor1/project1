68a8eb3871fc151e90e05a88c1024d6c2f7649c4
tylerchoe <tchoe1@ufl.edu>
2025-02-21 17:19:04 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -19,3 +19,30 @@ def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     """Takes the height of each point and adds the heights together. Then divides the heights by 2 to get the average
      height. Average height is then multiplied by the width or base to get the area of the trapezoid. Areas are then
      summed together"""
+
+
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc)->float:
+
+    difference = np.diff(x_vals)
+    height = func(x_vals[:-1])
+    approx = np.sum(difference * height)
+    return approx
+
+
+def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
+
+    h = np.diff(x_vals)[0]
+    f_vals = func(x_vals)
+
+    integral = f_vals[0] + f_vals[-1]
+    integral += 4 * np.sum(f_vals[1:-1:2])
+    integral += 2 * np.sum(f_vals[2:-1:2])
+
+    return (h / 3) * integral
+
+import numpy as np
+func = np.sin
+x_vals = np.linspace(0, np.pi, 10001)
+simpson_sum = simpson(x_vals, func)
+print(f"Simpson's: {simpson_sum}")
+
