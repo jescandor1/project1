2c62ac40dc196b269d93769bc2fa463dc5b6b37d
jescandor1 <jescandor1@ufl.edu>
2025-02-21 18:04:23 -0500

    edited get_barycentric_coordinates

--- barycentric.py
+++ barycentric.py
@@ -6,7 +6,7 @@ def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinate
     :param point_coordinates: 1d array (x, y)
     :return: 1d array (lambda1, lambda2, lambda3)
     """
-    triangle_coordinates = np.hstack((triangle_coordinates, np.ones(3, 1)))
+    triangle_coordinates = np.hstack((triangle_coordinates, np.ones((3, 1))))
     point_coordinates = np.append(point_coordinates, 1)
     barycentric_coordinates = np.linalg.solve(triangle_coordinates, point_coordinates)
     return barycentric_coordinates
