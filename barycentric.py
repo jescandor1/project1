132667902731ee99ae50a84d08d2df8c2b77a4a5
tylerchoe <tchoe1@ufl.edu>
2025-02-21 17:15:33 -0500

    Update barycentric.py

--- barycentric.py
+++ barycentric.py
@@ -19,3 +19,27 @@ def get_cartesian_coordinates (triangle_coordinates: np.array, barycentric_coord
     """Cartesian coordinates is the dot product of a 2x3 matrix and a 1x3 matrix to make a 1x2 matrix (x,y), also referred
     to as cartesian/point coordinates"""
     return cartesian_coordinates # (x,y)
+    
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc)->float:
+
+    difference = np.diff(x_vals)
+    height = func(x_vals[:-1])
+    approx = np.sum(difference * height)
+    return approx
+
+def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
+
+    h = np.diff(x_vals)[0]
+    f_vals = func(x_vals)
+
+    integral = f_vals[0] + f_vals[-1]
+    integral += 4 * np.sum(f_vals[1:-1:2])
+    integral += 2 * np.sum(f_vals[2:-1:2])
+
+    return (h / 3) * integral
+
+import numpy as np
+func = np.sin
+x_vals = np.linspace(0, np.pi, 10001)
+simpson_sum = simpson(x_vals, func)
+print(f"Simpson's: {simpson_sum}")
