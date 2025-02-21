37ba603078650a566988afcfe8abe4c844c02736
zacharyducore <zacharyducore@ufl.edu>
2025-02-21 00:39:43 -0500

    added jupyter file to repository

--- barycentric.py
+++ barycentric.py
@@ -1,4 +1,14 @@
+import numpy as np
 
+def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinates: np.array ) -> np.array:
+    """
+    :param triangle_coordinates: 2x3 array (coeff matrix)
+    :param point_coordinates: 1d array (x, y)
+    :return: 1d array (lambda1, lambda2, lambda3)
+    """
+    triangle_coordinates += [1, 1, 1]
+    point_coordinates += [1]
+    barycentric_coordinates = np.linalg.solve(triangle_coordinates, point_coordinates)
 
 def get_cartesian_coordinates (triangle_coordinates: np.array, barycentric_coordinates: np.array)-> np.array:
     """Triangle_coordinates are a 2x3 array (coeff matrix), Barycentric_coordinates are a 1d array (lamda1, lamda2,
