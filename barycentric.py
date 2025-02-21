17b1237f3fdb7440d5af4607ba85f0265a3ea330
jescandor1 <jescandor1@ufl.edu>
2025-02-21 18:29:29 -0500

    edited barycentric

--- barycentric.py
+++ barycentric.py
@@ -6,19 +6,20 @@ def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinate
     :param point_coordinates: 1d array (x, y)
     :return: 1d array (lambda1, lambda2, lambda3)
     """
-    triangle_coordinates = np.hstack((triangle_coordinates, np.ones((3, 1))))
-    point_coordinates = np.append(point_coordinates, 1)
-    barycentric_coordinates = np.linalg.solve(triangle_coordinates.T, point_coordinates)
+    homogeneous_coordinates = np.hstack((triangle_coordinates, np.ones((3, 1))))
+    point_homogeneous = np.append(point_coordinates, 1)
+    barycentric_coordinates = np.linalg.solve(homogeneous_coordinates.T, point_homogeneous)
     return barycentric_coordinates
 
 def get_cartesian_coordinates (triangle_coordinates: np.array, barycentric_coordinates: np.array)-> np.array:
     """Triangle_coordinates are a 2x3 array (coeff matrix), Barycentric_coordinates are a 1d array (lamda1, lamda2,
     lamda3)
     """
-    cartesian_coordinates = np.dot(barycentric_coordinates, triangle_coordinates)
+    homogeneous_coordinates = np.hstack((triangle_coordinates, np.ones((3, 1))))
+    cartesian_coordinates = np.dot(barycentric_coordinates, homogeneous_coordinates)
     """Cartesian coordinates is the dot product of a 2x3 matrix and a 1x3 matrix to make a 1x2 matrix (x,y), also referred
     to as cartesian/point coordinates"""
-    return cartesian_coordinates # (x,y)
+    return cartesian_coordinates[:2] # (x,y)
     
 def is_inside_triangle (triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> bool:
     barycentric_coordinates = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
